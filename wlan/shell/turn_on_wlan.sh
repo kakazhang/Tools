0dcbb27bcf3c3dc7bb48466ac4d498dae2cd95ad
zhanglin <zhanglin3406@gmail.com>
2020-11-23 21:50:42 +0800

    update turn wifi on shell script

--- wlan/shell/turn_on_wlan.sh
+++ wlan/shell/turn_on_wlan.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+sleep 4
 #insmode sdio wlan driver
-insmod /home/root/driver/rtl8189/8189fs.ko
+/sbin/insmod /lib/modules/wlan/8189fs.ko
 
 #disable rfkill
-rfkill unblock wifi
+/usr/sbin/rfkill unblock wifi
 
+sleep 4
 #turn wlan0 up
-ifconfig wlan0 up
+/sbin/ifconfig wlan0 up
 
-sleep 1
+sleep 2
 
-wpa_supplicant -Dnl80211 -c /etc/wpa_supplicant.conf -i wlan0 &
+/bin/wpa_supplicant -Dnl80211 -c /etc/wpa_supplicant.conf -i wlan0 &
+
+sleep 4
+/sbin/udhcpc -i wlan0
